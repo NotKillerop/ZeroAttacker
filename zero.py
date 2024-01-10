d50e93eeed56c1297e4d211d800c8c939575f2d3
NotKillerop <109949802+NotKillerop@users.noreply.github.com>
2024-01-10 23:09:00 +0530

    Add files via upload

---
+++ zero.py
@@ -0,0 +1,46 @@
+import sys,os
+from colorama import Fore
+
+print(Fore.MAGENTA+"""
+――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
+ _______  _______  _______  _______         _______ __________________ _______  _______  _        _______  ______    
+/ ___   )(  ____ \(  ____ )(  ___  )       (  ___  )\__   __/\__   __/(  ___  )(  ____ \| \    /\(  ____ \(  ____ )  
+\/   )  || (    \/| (    )|| (   ) |       | (   ) |   ) (      ) (   | (   ) || (    \/|  \  / /| (    \/| (    )|  
+    /   )| (__    | (____)|| |   | | _____ | (___) |   | |      | |   | (___) || |      |  (_/ / | (__    | (____)|
+   /   / |  __)   |     __)| |   | |(_____)|  ___  |   | |      | |   |  ___  || |      |   _ (  |  __)   |     __)
+  /   /  | (      | (\ (   | |   | |       | (   ) |   | |      | |   | (   ) || |      |  ( \ \ | (      | (\ (   
+ /   (_/\| (____/\| ) \ \__| (___) |       | )   ( |   | |      | |   | )   ( || (____/\|  /  \ \| (____/\| ) \ \__
+(_______/(_______/|/   \__/(_______)       |/     \|   )_(      )_(   |/     \|(_______/|_/    \/(_______/|/   \__/
+                                                                                                                   
+――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――――
+                                                                 
+BY: Zero Offens Security
+""")
+
+def display_menu():
+    print(Fore.GREEN + """
+    1: Zero-Tool (Hacking Tools)      | 2: Zero-Paid-Tools
+    3: Info (about-us)
+    """)
+
+def execute_command(command):
+    if command == '1':
+        os.system('cmd /k "python Zero-Tool/zero-tool.py"' if os.name == 'nt' else 'python Zero-Tool/zero-tool.py')
+    elif command == '2':
+        print(Fore.RED + 'This option is not available yet! Coming soon...')
+        #os.system('cmd /k "python Zero-Web-Hacktool/web_bugger.py"')
+    elif command == '3':
+        os.system('cmd /k "python info.py"' if os.name == 'nt' else 'python info.py')
+
+        display_menu()
+    else:
+        print('Invalid option! Please choose the correct one.')
+
+while True:
+    display_menu()
+    command = input('> ')
+
+    if command.lower() == 'exit':
+        break
+
+    execute_command(command)
